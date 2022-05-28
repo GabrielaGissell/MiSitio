aae61108a2953ba00b225f2b1d1d6b9a52d9ec66
GabrielaGissell <1217100910@alumnos.utng.edu.mx>
2022-05-28 16:24:37 -0500

    first commit

---
+++ js/prom_1.js
@@ -0,0 +1,22 @@
+console.log('imprimir resultado: ');
+
+//Funciones en JavaScript
+function sumarUno(numero, callback){
+    //Simulando función asincrona
+    setTimeout (function(){
+        callback(numero + 1);
+    }, 1500);
+
+
+}
+
+sumarUno(5, function(nuevoValor){
+    console.log(nuevoValor);
+    sumarUno(nuevoValor, function(nuevoValor2){
+        console.log(nuevoValor2);
+        sumarUno(nuevoValor2, function(nuevoValor3){
+            console.log(nuevoValor3);
+        });
+    });
+});
+
